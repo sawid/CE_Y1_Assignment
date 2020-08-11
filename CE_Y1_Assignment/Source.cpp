7f9c8a3f5228283b3c970c5a29ebc7dc7a1d07f4
sawid <zasawidza@gmail.com>
2020-08-11 13:37:52 +0700

    Day 1 - Test Input

--- CE_Y1_Assignment/Source.cpp
+++ CE_Y1_Assignment/Source.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
+	int x;
+	scanf_s("%d",&x);
 	printf("Testing\n");
-	printf("To Use Repository");
-	printf("To Use Repository Time 2");
+	printf("To Use Repository\n");
+	printf("To Use Repository Time 2\n");
+	printf("To Use Repository Time Text is %d",x);
 	return 0;
 }
